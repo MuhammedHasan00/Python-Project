4ab00739ecaec63cbd64cb4bdc3a74d6fa42cfec
Muhammed <muhammed.husam.hasan@gmail.com>
2024-05-13 16:13:52 +0100

    additional error handling on reports

--- report.py
+++ report.py
@@ -3,25 +3,45 @@ from connect import *
 #subroutines
 
 def genre():
-    filmGenre = input("Enter Genre (Comedy, Action, Crime, Test, Fantasy, Animation, Fighting): ").title()
-    cursor.execute(f"SELECT * FROM tblfilms WHERE genre = '{filmGenre}'")
-    row = cursor.fetchall()
-    for aRecord in row:
-        print(aRecord)    
+    try:
+        filmGenre = input("Enter Genre (Comedy, Action, Crime, Test, Fantasy, Animation, Fighting): ").title()
+        cursor.execute(f"SELECT * FROM tblfilms WHERE genre = '{filmGenre}'")
+
+        if cursor.fetchone() is None:
+            print(f"Films with genre {filmGenre} not found! Please enter one of the following (Comedy, Action, Crime, Test, Fantasy, Animation, Fighting)")
+        else:
+            row = cursor.fetchall()
+            for aRecord in row:
+                print(aRecord)  
+    except (sql.OperationalError, ValueError) as e:
+        print(f"Records not found, {e}")  
 
 def year():
-    filmYear = int(input("Enter Year: "))
-    cursor.execute(f"SELECT * FROM tblfilms WHERE yearReleased = '{filmYear}'")
-    row = cursor.fetchall()
-    for aRecord in row:
-        print(aRecord)    
+    try:
+        filmYear = int(input("Enter Year: "))
+        cursor.execute(f"SELECT * FROM tblfilms WHERE yearReleased = '{filmYear}'")
+        if cursor.fetchone() is None:
+            print(f"Cannot find any films from the year {filmYear}")
+        else:
+            row = cursor.fetchall()
+            for aRecord in row:
+                print(aRecord)    
+    except (sql.OperationalError, ValueError) as e:
+        print(f"Records not found, {e}") 
 
 def rating():
-    filmRating = input("Enter Rating (PG, R, G): ").upper()
-    cursor.execute(f"SELECT * FROM tblfilms WHERE rating = '{filmRating}'")
-    row = cursor.fetchall()
-    for aRecord in row:
-        print(aRecord)    
+    try:
+        filmRating = input("Enter Rating (PG, R, G): ").upper()
+        ratingOptions = ["PG","R", "G"]
+        if filmRating not in ratingOptions:
+            print(f"cannot find films with rating {filmRating}, please try one of the following ratings (PG, R, G)!")
+        else:
+            cursor.execute(f"SELECT * FROM tblfilms WHERE rating = '{filmRating}'")
+            row = cursor.fetchall()
+            for aRecord in row:
+                print(aRecord)
+    except (sql.OperationalError, ValueError) as e:
+        print(f"Records not found, {e}")     
 
 if __name__ == "__main__":
     genre()
